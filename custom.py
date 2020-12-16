f8b0521116b6487213e18e37c89b3a6e57a5ed98
LyricLy <gulliverhanson@gmail.com>
2020-12-16 22:55:06 +1300

    lol wtf

--- custom.py
+++ custom.py
@@ -5,7 +5,7 @@ import time
 
 languages = {
     "brainfuck": "tritium -b -z .code",
-    "letra": "python ../letra.py .code",
+    "letra": "cd /home/lyricly/aoccy && python ../letra.py .code",
 }
 
 async def execute(lang, code, input_):
@@ -17,7 +17,7 @@ async def execute(lang, code, input_):
     sh = await asyncio.create_subprocess_shell(languages[lang], stdin=asyncio.subprocess.PIPE, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
     try:
         stdout, stderr = await asyncio.wait_for(sh.communicate(input_.encode()), timeout=120)
-        return stdout, stderr + f"\nReal time: {time.perf_counter()-start:.3f} s\na\na\na\n0".encode(), b""
+        return stdout, stderr + f"\nReal time: {time.perf_counter()-start:.3f} s\na\na\na\n{sh.returncode}".encode(), b""
     except asyncio.TimeoutError:
         # if we try to read the data, we're likely just to deadlock
         return b"", b"", b"Execution timed out after 120s."
