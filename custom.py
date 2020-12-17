8d2030a194fd651fc144361da7d880b956b7028a
LyricLy <gulliverhanson@gmail.com>
2020-12-17 13:40:53 +1300

    God dammit Heavpoot

--- custom.py
+++ custom.py
@@ -16,8 +16,8 @@ async def execute(lang, code, input_):
     start = time.perf_counter()
     sh = await asyncio.create_subprocess_shell(languages[lang], stdin=asyncio.subprocess.PIPE, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
     try:
-        stdout, stderr = await asyncio.wait_for(sh.communicate(input_.encode()), timeout=120)
+        stdout, stderr = await asyncio.wait_for(sh.communicate(input_.encode()), timeout=15)
         return stdout, stderr + f"\nReal time: {time.perf_counter()-start:.3f} s\na\na\na\n{sh.returncode}".encode(), b""
     except asyncio.TimeoutError:
         # if we try to read the data, we're likely just to deadlock
-        return b"", b"", b"Execution timed out after 120s."
+        return b"", b"", b"Execution timed out after 15s."
