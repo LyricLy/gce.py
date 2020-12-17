b9b2cbdbacd3d192dee80ba0ed0368dc9993c4da
LyricLy <gulliverhanson@gmail.com>
2020-12-17 14:51:19 +1300

    Fix race condition

--- custom.py
+++ custom.py
@@ -4,17 +4,18 @@ import time
 
 
 languages = {
-    "brainfuck": "tritium -b -z .code",
-    "letra": "python ../letra.py .code",
+    "brainfuck": "tritium -b -z {code}",
+    "letra": "python ../letra.py {code}",
 }
 
 async def execute(lang, code, input_):
     if isinstance(code, str):
         code = code.encode()
-    with open(".code", "wb") as f:
+    filename = f".code_{hash(code)}"
+    with open(filename, "wb") as f:
         f.write(code)
     start = time.perf_counter()
-    sh = await asyncio.create_subprocess_shell(languages[lang], stdin=asyncio.subprocess.PIPE, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    sh = await asyncio.create_subprocess_shell(languages[lang].format(code=filename), stdin=asyncio.subprocess.PIPE, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
     try:
         stdout, stderr = await asyncio.wait_for(sh.communicate(input_.encode()), timeout=15)
         return stdout, stderr + f"\nReal time: {time.perf_counter()-start:.3f} s\na\na\na\n{sh.returncode}".encode(), b""
